cba59b286c542cdc248910436f390c78b985a93f
blakerom <blakeromero001@gmail.com>
2020-09-23 00:59:43 -0700

    working on runnning state issues

--- src/components/todo/form.js
+++ src/components/todo/form.js
@@ -6,7 +6,7 @@ function TodoForm(props) {
   const [item, setItem] = useState({});
 
   const handleInputChange = e => {
-    setState({ ...item, [e.target.name]: e.target.value } );
+    setItem({ ...item, [e.target.name]: e.target.value } );
   };
 
   const handleSubmit = (e) => {
--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -1,27 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TodoForm from './form.js';
 import TodoList from './list.js';
 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
 import './todo.scss';
 
-function ToDo() {
+function ToDo(props) {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: [],
-    };
-  }
+  const [ list, setList ] = useState([]);
 
-  addItem = (item) => {
+  const addItem = (item) => {
     item._id = Math.random();
     item.complete = false;
-    this.setState({ list: [...this.state.list, item]});
+    setList([ ...list, item]);
   };
 
-  toggleComplete = id => {
+  const toggleComplete = id => {
 
-    let item = this.state.list.filter(i => i._id === id)[0] || {};
+    let item = list.filter(i => i._id === id)[0] || {};
 
     if (item._id) {
       item.complete = !item.complete;
@@ -31,8 +31,8 @@ function ToDo() {
 
   };
 
-  componentDidMount() {
-    let list = [
+  useEffect(() => {
+    let newList = [
       { _id: 1, complete: false, text: 'Clean the Kitchen', difficulty: 3, assignee: 'Person A'},
       { _id: 2, complete: false, text: 'Do the Laundry', difficulty: 2, assignee: 'Person A'},
       { _id: 3, complete: false, text: 'Walk the Dog', difficulty: 4, assignee: 'Person B'},
@@ -40,34 +40,40 @@ function ToDo() {
       { _id: 5, complete: false, text: 'Take a Nap', difficulty: 1, assignee: 'Person B'},
     ];
 
-    this.setState({list});
-  }
+    setList(newList);
+  }, []);
 
-  render() {
-    return (
-      <>
-        <header>
-          <h2>
-          There are {this.state.list.filter(item => !item.complete).length} Items To Complete
-          </h2>
-        </header>
+  useEffect(() => {
+    document.title = `To Do List: ${list.length}`
+  }, [list]);
 
-        <section className="todo">
+  return (
+    <>
+      <header>
+        <Navbar>
+          <Nav>
+            <h2>
+              ToDo List Manager ({list.length})
+            </h2>
+          </Nav>
+        </Navbar>
+      </header>
 
-          <div>
-            <TodoForm handleSubmit={this.addItem} />
-          </div>
+      <section className="todo">
 
-          <div>
-            <TodoList
-              list={this.state.list}
-              handleComplete={this.toggleComplete}
-            />
-          </div>
-        </section>
-      </>
-    );
-  }
+        <div>
+          <TodoForm handleSubmit={addItem} />
+        </div>
+
+        <div>
+          <TodoList
+            list={list}
+            handleComplete={toggleComplete}
+          />
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default ToDo;
